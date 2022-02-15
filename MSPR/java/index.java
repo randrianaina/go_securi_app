1b2606a46dcc1c118f074fdc43c60ab2de3c8d00
randrianaina <25117692+randrianaina@users.noreply.github.com>
2022-02-15 15:21:07 +0100

    first commit

--- MSPR/java/index.java
+++ MSPR/java/index.java
@@ -10,7 +10,7 @@ public class index {static String[] args2;
 
 
 
-        var path = Paths.get("MSPR/java", "staff.txt");
+        var path = Paths.get("MSPR/java/database", "staff.txt");
         FileReader fr = new FileReader(path.toString());
         BufferedReader br = new BufferedReader(fr);
         Scanner sc = new Scanner(fr);
